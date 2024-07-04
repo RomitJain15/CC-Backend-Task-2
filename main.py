ae2c5d4442ef1297c5c5745f060a965d490a03a6
RomitJain15 <jainromit15@gmail.com>
2024-07-04 18:56:17 +0530

    Made small change in comment

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def read_root():
     response = JSONResponse(content=gpu_prices_map)
 
     '''
-      In the below lines I'm handling CORS error which occurs because I am opening index.html in browser which opens as a file domain and in that I'm trying to fetch data from HTTP domain which is restricted due to security policy. Hence in the following lines I am setting HTTP headers to manage CORS and telling the browser which domains are allowed to make requests to the server.
+      In the below lines I'm handling CORS error which occurs because I am opening index.html in browser which opens as a file domain and in that I'm trying to fetch data from HTTP domain which is restricted due to security policy. 
+      Hence in the following lines I am setting HTTP headers to manage CORS and telling the browser which domains are allowed to make requests to the server.
     '''
 
     response.headers["Access-Control-Allow-Origin"] = "*"  # This allows all domains to make requests.
